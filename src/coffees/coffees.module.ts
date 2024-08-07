dc06b9f151fcb5963a7c94dabf89957230d60fe0
KalPal <reznichenkomykola1@gmail.com>
2024-08-07 16:24:55 +0300

    Getting Started with Test Suites

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,38 +1,14 @@
-import { Module, Scope } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
-import { COFFEE_BRANDS } from './cofees.constants';
-import { ConfigModule } from '@nestjs/config';
-import coffeesConfig from 'src/config/coffees.config';
-
-class CacheManager {}
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Coffee, Flavor, Event]),
-    ConfigModule.forFeature(coffeesConfig),
-  ],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
-  providers: [
-    CoffeesService,
-    {
-      provide: COFFEE_BRANDS,
-      useFactory: async () => {
-        // имитируем подключение к чему-то асинхронному
-        const result = await Promise.resolve(['lviv cava', 'nescafe']);
-        console.log('connected to something');
-        return result;
-      },
-    },
-    {
-      provide: 'CACHE_MANAGER',
-      useClass: CacheManager,
-      scope: Scope.TRANSIENT,
-    },
-  ],
+  providers: [CoffeesService],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
@@ -6,10 +6,6 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
-import { Event } from 'src/events/entities/event.entity';
-import { COFFEE_BRANDS } from './cofees.constants';
-import { ConfigType } from '@nestjs/config';
-import coffeesConfig from 'src/config/coffees.config';
 
 @Injectable()
 export class CoffeesService {
@@ -19,13 +15,7 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly dataSource: DataSource,
-    @Inject(COFFEE_BRANDS) private coffeeBrands: string[],
-    @Inject(coffeesConfig.KEY)
-    coffeesConfiguration: ConfigType<typeof coffeesConfig>,
-  ) {
-    console.log(coffeesConfiguration.promo.cod);
-    console.log(coffeesConfiguration.foo);
-  }
+  ) {}
 
   async findAll(paginationQueryDto: PaginationQueryDto) {
     const { limit, offcet } = paginationQueryDto;
@@ -78,31 +68,6 @@ export class CoffeesService {
   async remove(id: number) {
     const coffee = await this.findOne(id);
     return await this.coffeeRepository.remove(coffee);
-  }
-
-  async recommendCoffee(coffee: Coffee): Promise<void> {
-    const queryRunner = this.dataSource.createQueryRunner();
-
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-
-    try {
-      coffee.recommendations++;
-
-      const recommendEvent = new Event();
-      recommendEvent.type = 'coffee';
-      recommendEvent.name = 'recommend_coffee';
-      recommendEvent.payload = { coffeeId: coffee.id };
-
-      await queryRunner.manager.save(coffee);
-      await queryRunner.manager.save(recommendEvent);
-
-      await queryRunner.commitTransaction();
-    } catch {
-      await queryRunner.rollbackTransaction();
-    } finally {
-      await queryRunner.release();
-    }
   }
 
   async preloadFlavor(name: string): Promise<Flavor> {
